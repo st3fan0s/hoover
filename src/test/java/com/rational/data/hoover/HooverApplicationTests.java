1801ff76a0f3b1cc7c22dd04081c48784cb23367
st3fan0s <stefanos.falangitis@gmail.com>
2024-10-08 00:05:06 +0300

    Initial commit - backbone of project

---
+++ src/test/java/com/rational/data/hoover/HooverApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rational.data.hoover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
